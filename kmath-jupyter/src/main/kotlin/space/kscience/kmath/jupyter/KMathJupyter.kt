554943c95857b2da24b929ddd223351f50a516f0
Iaroslav Postovalov <postovalovya@gmail.com>
2021-03-16 23:17:54 +0700

    Prototype of generic complex numbers

--- kmath-jupyter/src/main/kotlin/space/kscience/kmath/jupyter/KMathJupyter.kt
+++ kmath-jupyter/src/main/kotlin/space/kscience/kmath/jupyter/KMathJupyter.kt
@@ -12,7 +12,7 @@ import space.kscience.kmath.ast.rendering.FeaturedMathRendererWithPostProcess
 import space.kscience.kmath.ast.rendering.MathMLSyntaxRenderer
 import space.kscience.kmath.ast.rendering.renderWithStringBuilder
 import space.kscience.kmath.complex.Complex
-import space.kscience.kmath.complex.Quaternion
+//import space.kscience.kmath.complex.Quaternion
 import space.kscience.kmath.expressions.MST
 import space.kscience.kmath.expressions.MstRing
 import space.kscience.kmath.misc.PerformancePitfall
@@ -121,19 +121,20 @@ internal class KMathJupyter : JupyterIntegration() {
             })
         }
 
-        render<Complex> {
+        render<Complex<*>> {
             MstRing {
-                number(it.re) + number(it.im) * bindSymbol("i")
+                if (it.re is Number && it.im is Number)
+                    number(it.re) + number(it.im) * bindSymbol("i")
             }.toDisplayResult()
         }
 
-        render<Quaternion> {
-            MstRing {
-                number(it.w) +
-                        number(it.x) * bindSymbol("i") +
-                        number(it.x) * bindSymbol("j") +
-                        number(it.x) * bindSymbol("k")
-            }.toDisplayResult()
-        }
+//        render<Quaternion> {
+//            MstRing {
+//                number(it.w) +
+//                        number(it.x) * bindSymbol("i") +
+//                        number(it.y) * bindSymbol("j") +
+//                        number(it.z) * bindSymbol("k")
+//            }.toDisplayResult()
+//        }
     }
 }
